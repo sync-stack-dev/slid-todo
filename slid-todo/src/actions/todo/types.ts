2ffec07f3a0c60a58c9902d08fb914cb9ffbbe2d
catmaker <rzt0317@nate.com>
2024-11-18 22:54:32 +0900

    feat: Goal과 Todo 타입 정의 파일 생성
    types/goal.ts:
    - Goal 인터페이스 정의
    - GoalStatus 타입 정의
    - GoalWithTodos 인터페이스 정의
    
    types/todo.ts:
    - Todo 인터페이스 정의
    - TodoStatus 타입 정의
    - TodosResponse 인터페이스 정의

---
+++ slid-todo/src/actions/todo/types.ts
@@ -0,0 +1,29 @@
+// actions/todo/types.ts
+export interface Todo {
+  noteId: number;
+  done: boolean;
+  linkUrl: string;
+  fileUrl: string;
+  title: string;
+  id: number;
+  goal: {
+    id: number;
+    title: string;
+  };
+  userId: number;
+  teamId: string;
+  updatedAt: string;
+  createdAt: string;
+}
+
+export interface TodosResponse {
+  todos: Todo[];
+  nextCursor: number;
+  totalCount: number;
+}
+
+export interface GetTodosParams {
+  goalId: number;
+  cursor?: number;
+  size?: number;
+}
