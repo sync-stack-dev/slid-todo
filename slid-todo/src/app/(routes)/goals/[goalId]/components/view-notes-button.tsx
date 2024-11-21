95c44fe5476e944f13f71d90665890b8e0c92f7d
catmaker <rzt0317@nate.com>
2024-11-21 19:07:32 +0900

    design: 노트 모아보기 버튼 UI 개선
    - 노트 모아보기 버튼에 화살표 아이콘 추가
    - 레이아웃 구조 개선

--- slid-todo/src/app/(routes)/goals/[goalId]/components/view-notes-button.tsx
+++ slid-todo/src/app/(routes)/goals/[goalId]/components/view-notes-button.tsx
@@ -1,11 +1,19 @@
 import { Note } from "../../../../../../public/svgs";
+import { ChevronRight } from "lucide-react";
+import Link from "next/link";
 
 const ViewNotesButton = () => {
   return (
-    <div className="flex gap-2 items-center max-w-[1200px] max-h-[136px] bg-white rounded-lg py-4 px-6 mt-4 bg-blue-100 text-lg font-bold">
-      <Note className="w-6 h-6" />
-      <p className="text-sm font-semibold">노트 모아보기</p>
-    </div>
+    <Link
+      href={`/`}
+      className="flex items-center justify-between max-w-[1200px] max-h-[136px] bg-white rounded-lg py-4 px-6 mt-4 bg-blue-100 text-lg font-bold"
+    >
+      <div className="flex items-center gap-2">
+        <Note className="w-6 h-6" />
+        <p className="text-sm font-semibold">노트 모아보기</p>
+      </div>
+      <ChevronRight className="w-4 h-4" />
+    </Link>
   );
 };
 
