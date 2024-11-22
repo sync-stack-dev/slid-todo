b2a184cd93f372a69a8e20d2bbbb162bf18c94a8
s-ggul <k0s0a7@gmail.com>
2024-11-22 15:33:54 +0900

    [feature] 목표 별 note-card 구현

--- slid-todo/src/app/(routes)/note/components/goal-note-card.tsx
+++ slid-todo/src/app/(routes)/note/components/goal-note-card.tsx
@@ -1,4 +1,7 @@
-import { Note } from "../types";
+import { Note } from "@/types/note";
+import NoteList from "../../../../../public/svgs/note-list";
+import { Separator } from "@/components/ui/separator";
+import GoalMeatballBtn from "./goal-meatball-btn";
 
 interface GoalNoteCardProps {
   note: Note;
@@ -6,9 +9,23 @@ interface GoalNoteCardProps {
 
 const GoalNoteCard = ({ note }: GoalNoteCardProps) => {
   return (
-    <>
-      <div>{note.id}</div>
-    </>
+    <div className="grid gap-2.5 grid-cols-1 bg-white w-1/2 p-[24px] border-[1px] border-slate-100 rounded-xl">
+      <div className="flex justify-between">
+        <NoteList />
+        <GoalMeatballBtn noteId={note.id} />
+      </div>
+      <div className="text-slate-800 text-lg">{note.title}</div>
+      <Separator className="border-[1px]" />
+      <div className="flex text-xs text-slate-700 items-center">
+        <div
+          className="bg-slate-100 rounded-lg mr-2 flex justify-center items-center px-[3px] py-[2px]
+        "
+        >
+          To do
+        </div>
+        <div className=" ">{note.todo.title}</div>
+      </div>
+    </div>
   );
 };
 
