0e7176994a28cf90fa6a864e3bb46813d9f84f2f
s-ggul <k0s0a7@gmail.com>
2024-11-22 23:32:41 +0900

    [feat] note card 구현 및 note 상세 조회 sheet 초기 컴포넌트

--- slid-todo/src/app/(routes)/note/components/note-card.tsx
+++ slid-todo/src/app/(routes)/note/components/note-card.tsx
@@ -1,18 +1,19 @@
 import { Note } from "@/types/note";
 import NoteList from "../../../../../public/svgs/note-list";
 import { Separator } from "@/components/ui/separator";
-import GoalMeatballBtn from "./goal-meatball-btn";
+import NoteMeatballBtn from "./note-meatball-btn";
+import { forwardRef } from "react";
 
-interface GoalNoteCardProps {
+interface NoteCardProps {
   note: Note;
 }
 
-const GoalNoteCard = ({ note }: GoalNoteCardProps) => {
+const NoteCard = ({ note }: NoteCardProps) => {
   return (
-    <div className="grid gap-2.5 grid-cols-1 bg-white w-1/2 p-[24px] border-[1px] border-slate-100 rounded-xl">
+    <div className="grid gap-2.5 grid-cols-1 bg-white w-1/2 p-[24px] border-[1px] border-slate-100 rounded-xl cursor-pointer transform transition-transform duration-200 hover:scale-105 hover:bg-slate-50">
       <div className="flex justify-between">
         <NoteList />
-        <GoalMeatballBtn noteId={note.id} />
+        <NoteMeatballBtn noteId={note.id} />
       </div>
       <div className="text-slate-800 text-lg">{note.title}</div>
       <Separator className="border-[1px]" />
@@ -29,4 +30,4 @@ const GoalNoteCard = ({ note }: GoalNoteCardProps) => {
   );
 };
 
-export default GoalNoteCard;
+export default NoteCard;
