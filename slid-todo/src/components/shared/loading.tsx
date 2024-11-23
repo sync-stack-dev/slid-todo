8a0cf393c2b7a9c3e97674cda3b82c8693fc2f3b
catmaker <rzt0317@nate.com>
2024-11-24 02:31:33 +0900

    [SCRUM-47] feat: Loading 공통 컴포넌트 생성

---
+++ slid-todo/src/components/shared/loading.tsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+  return (
+    <div className="flex items-center justify-center h-full">
+      <div className="animate-spin rounded-full h-8 w-8 border-t-2 border-b-2 border-primary" />
+    </div>
+  );
+};
+
+export default Loading;
