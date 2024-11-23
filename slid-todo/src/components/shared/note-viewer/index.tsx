1be3f71139dc9055938263413d6c084606ce8288
catmaker <rzt0317@nate.com>
2024-11-24 02:29:33 +0900

    [SCRUM-46] refactor: More menu 공통 컴포넌트화 및 Tooltip 추가
    - More menu를 shared 컴포넌트로 이동하여 재사용성 향상
    - Shadcn Tooltip 컴포넌트 적용
    - 더보기 버튼 hover 시 툴팁 표시
    - 컴포넌트 JSDoc 문서화 추가

--- slid-todo/src/components/shared/note-viewer/index.tsx
+++ slid-todo/src/components/shared/note-viewer/index.tsx
@@ -1,3 +1,36 @@
+/**
+ * 할 일의 노트를 보여주는 시트 컴포넌트입니다.
+ * 데이터 페칭은 상위 컴포넌트에서 처리해야 합니다.
+ *
+ * @example
+ * 상위 컴포넌트에서 데이터 패칭 후 전달
+ * const ParentComponent = () => {
+ *   const [isOpen, setIsOpen] = useState(false);
+ *   데이터 페칭 예시
+ *   const { data: todo } = useTodoQuery(todoId);
+ *   const { data: noteData } = useNoteQuery(todoId);
+ *
+ *   return (
+ *     <>
+ *       <button onClick={() => setIsOpen(true)}>
+ *         {todo.title}
+ *       </button>
+ *       <NoteViewer
+ *         isOpen={isOpen}
+ *         onOpenChange={setIsOpen}
+ *         todo={todo}
+ *         noteData={noteData}
+ *       />
+ *     </>
+ *   );
+ * };
+ *
+ * @param props.isOpen - 시트의 열림/닫힘 상태
+ * @param props.onOpenChange - 시트 상태 변경 핸들러
+ * @param props.todo - 상위 컴포넌트에서 페칭한 할 일 데이터
+ * @param props.noteData - 상위 컴포넌트에서 페칭한 노트 데이터
+ */
+
 import { Sheet, SheetContent, SheetTitle } from "@/components/ui/sheet";
 import { Note } from "@/types/note";
 import { Todo } from "@/types/todo";
