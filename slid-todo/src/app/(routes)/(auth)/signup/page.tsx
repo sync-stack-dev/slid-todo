71776841ba935fe7d4a5a298d15c6e79dc88aead
jykim <wddoods015@gmail.com>
2024-11-23 23:47:12 +0900

    [feature][SCRUM-18]  회원가입 페이지
    form, submit button, input 컴포넌트 & 유효성 검사 추가, 비밀번호 숨김/확인 기능-password-input 추가

--- slid-todo/src/app/(routes)/(auth)/signup/page.tsx
+++ slid-todo/src/app/(routes)/(auth)/signup/page.tsx
@@ -1,5 +1,20 @@
-const SignupComponent = () => {
-  return <div>SignupComponent</div>;
-};
+import SignupForm from "./components/Signup-form";
+import Image from "next/image";
+import Container from "@/components/ui/container";
 
-export default SignupComponent;
+const SignupPage = () => {
+  return(
+    <div className="bg-white w-full">
+      <Container>
+        <div className="flex justify-center mb-8 flex-col items-center h-screen">
+          <div className="mb-8">
+            <Image src="/imgs/logo.png" alt="Slid to-do" width={270} height={89} priority />
+          </div>
+          <SignupForm />
+          </div>
+     </Container>
+    </div>
+  );
+}
+
+export default SignupPage;
