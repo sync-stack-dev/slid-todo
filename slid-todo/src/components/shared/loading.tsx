d3a6bf31e2256dd0b4fa8d0cb6ce4e6fa195bf03
신지수 <114233139+catmaker@users.noreply.github.com>
2024-11-24 17:47:14 +0900

    feat: todo-list 공통 컴포넌트 작업

--- slid-todo/src/components/shared/loading.tsx
+++ slid-todo/src/components/shared/loading.tsx
@@ -1,6 +1,6 @@
 const Loading = () => {
   return (
-    <div className="flex items-center justify-center h-full">
+    <div className="flex justify-center items-center h-screen">
       <div className="animate-spin rounded-full h-8 w-8 border-t-2 border-b-2 border-primary" />
     </div>
   );
