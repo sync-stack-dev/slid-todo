55c918f30b250306afed9756d6d7696090f67f5b
s-ggul <k0s0a7@gmail.com>
2024-11-25 14:28:24 +0900

    [update] 컴포넌트 네이밍 변경

--- slid-todo/src/app/(routes)/note/page.tsx
+++ slid-todo/src/app/(routes)/note/page.tsx
@@ -1,14 +1,13 @@
-import GoalNoteList from "./components/goal-note-list";
-import GoalTitle from "./components/goal-title";
+import NoteList from "./components/note-list";
+import NoteGoalTitle from "./components/note-goal-title";
 
 const NotePage = () => {
   return (
     <>
       <div className="w-screen h-screen bg-[#F1F5F9] px-36 py-10">
         <div className="text-lg text-slate-900 mb-5">노트 모아보기</div>
-
-        <GoalTitle />
-        <GoalNoteList />
+        <NoteGoalTitle />
+        <NoteList />
       </div>
     </>
   );
