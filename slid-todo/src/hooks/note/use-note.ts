95b8e51d599b495eacf4728b61113737e76b1e97
s-ggul <k0s0a7@gmail.com>
2024-11-25 17:38:20 +0900

    [feat] use-note 훅 동적 queryKey 추가

--- slid-todo/src/hooks/note/use-note.ts
+++ slid-todo/src/hooks/note/use-note.ts
@@ -1,10 +1,20 @@
 import { instance } from "@/lib/axios";
-import { NotesResponse } from "@/types/note";
+import { Note, NotesResponse } from "@/types/note";
 import { useQuery } from "@tanstack/react-query";
+
+export const useNoteById = (noteId: number) => {
+  return useQuery<Note>({
+    queryKey: ["notes", noteId],
+    queryFn: async () => {
+      const response = await instance.get<Note>(`/notes/${noteId}`);
+      return response.data;
+    },
+  });
+};
 
 export const useNoteList = (goalId: number) => {
   return useQuery<NotesResponse>({
-    queryKey: ["notes", "list"],
+    queryKey: ["notes", "list", goalId],
     queryFn: async () => {
       const size = 40;
       const response = await instance.get<NotesResponse>("/notes", {
