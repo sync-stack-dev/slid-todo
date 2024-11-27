ff747b9ec3244248cc616e9ad1680717b7e38be8
jykim <wddoods015@gmail.com>
2024-11-27 18:45:43 +0900

    [SCRUM-56]feat:  대시보드 - 최근 할 일 블럭 구현
    - use-todos.ts 중복 파일 제거
    - dashboard/components/ 하위 블럭별 컴포넌트 폴더 분리

---
+++ slid-todo/src/app/(routes)/dashboard/components/goal-todo-block/board-goal-todo.tsx
@@ -0,0 +1,11 @@
+// 목표별 할일 블럭
+
+const GoalToDo = () => {
+    return(
+        <div className="bg-white w-full h-[calc(100vh-120px)] rounded-2xl flex flex-col p-4">
+
+        </div>      
+    );
+};
+
+export default GoalToDo;
--- slid-todo/src/app/(routes)/dashboard/page.tsx
+++ slid-todo/src/app/(routes)/dashboard/page.tsx
@@ -1,10 +1,19 @@
-
+import RecentToDo from "./components/recent-todo-block/recent-todo";
+import GoalToDo from "./components/goal-todo-block/board-goal-todo";
+import MyProgress from "./components/progress-block/board-progress";
 
 const DashBoard = () => {
     return(
-        <div className="bg-white w-full">
-            DashBoard page 입니다.
-        </div>
+       <div className="bg-[#F1F5F9] w-full flex justify-center">
+            <div className="w-[90%]  flex flex-col gap-4 p-2">
+                <h2 className="text-[18px]">대시보드</h2>
+                <div className="w-full flex gap-4">
+                    <RecentToDo />
+                    <MyProgress />
+                </div>
+                <GoalToDo />
+            </div>
+       </div> 
     );
 };
 
