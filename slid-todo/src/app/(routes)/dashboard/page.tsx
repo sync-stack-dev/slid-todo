e42f39efcc13dd4466d5c19145d2dd657922e250
catmaker <rzt0317@nate.com>
2024-12-03 22:30:40 +0900

    feat: isLoading만 불러와 전체 컴포넌트 로딩 상태 적용

--- slid-todo/src/app/(routes)/dashboard/page.tsx
+++ slid-todo/src/app/(routes)/dashboard/page.tsx
@@ -1,8 +1,23 @@
+"use client";
 import RecentToDo from "./components/recent-todo-block/recent-todo";
 import GoalToDo from "./components/goal-todo-block/goal-todo";
 import MyProgress from "./components/progress-block/board-progress";
+import { Loading } from "@/components/shared/loading";
+import { useRecentTodos } from "@/hooks/todo/use-todos";
+import { useProgress } from "@/hooks/goals/use-dashboard-goals";
+import { useGoalListInfinite } from "@/hooks/goals/use-dashboard-goals";
 
 const DashBoard = () => {
+  const { isLoading: isRecentLoading } = useRecentTodos();
+  const { isLoading: isProgressLoading } = useProgress();
+  const { isLoading: isGoalLoading } = useGoalListInfinite();
+
+  const isLoading = isRecentLoading || isProgressLoading || isGoalLoading;
+
+  if (isLoading) {
+    return <Loading fullScreen={false} size={150} />;
+  }
+
   return (
     <div className="container px-4 py-6">
       <div className="rounded-2xl w-full mx-auto h-full lg:mx-16 flex flex-col gap-4">
