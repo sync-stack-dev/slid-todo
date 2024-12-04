6671b5032983a33c71acb8f4a0788d229df2c169
catmaker <rzt0317@nate.com>
2024-12-04 14:01:01 +0900

    design: 전체적인 디자인 수정 및 로고 + svg 다크모드 수정

--- slid-todo/public/svgs/index.ts
+++ slid-todo/public/svgs/index.ts
@@ -1,5 +1,7 @@
 import Goal from "./goal";
 import Note from "./note";
 import DashboardIcon from "./dashboard";
+import { LogoText, LogoTextDark } from "./logo-text";
+import LogoIcon from "./logo-icon";
 
-export { Goal, Note, DashboardIcon };
+export { Goal, Note, DashboardIcon, LogoText, LogoTextDark, LogoIcon };
--- slid-todo/src/components/shared/app-sidebar/components/app-sidebar-header.tsx
+++ slid-todo/src/components/shared/app-sidebar/components/app-sidebar-header.tsx
@@ -3,6 +3,7 @@ import AppLogo from "../../app-logo/app-logo";
 import { CollapseButton } from "./collapse-button";
 import Skeleton from "@/components/shared/skeleton";
 import { useUserQuery } from "@/stores/use-user-store";
+import { LogoText, LogoTextDark, LogoIcon } from "@/public/svgs";
 
 export const AppSidebarHeader = () => {
   const { isLoading } = useUserQuery();
@@ -21,7 +22,14 @@ export const AppSidebarHeader = () => {
   return (
     <div className="flex justify-between items-center px-2 py-2">
       <Link href="/dashboard">
-        <AppLogo />
+        <div className="dark:hidden flex items-center gap-2">
+          <LogoIcon />
+          <LogoText />
+        </div>
+        <div className="hidden dark:flex items-center gap-2">
+          <LogoIcon />
+          <LogoTextDark />
+        </div>
       </Link>
       <CollapseButton />
     </div>
