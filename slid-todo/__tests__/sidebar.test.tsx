5e4f9c6a028ec0523cc9ddde6944da7964daed30
신지수 <114233139+catmaker@users.noreply.github.com>
2025-01-03 16:34:56 +0900

    fix: 기존 사이드바 테스트 제거

--- slid-todo/__tests__/sidebar.test.tsx
+++
@@ -1,9 +0,0 @@
-import { expect } from "@jest/globals";
-
-const add = (a: number, b: number) => a + b;
-
-describe("AppSidebar", () => {
-  it("test jest", () => {
-    expect(add(1, 2)).toBe(3);
-  });
-});
